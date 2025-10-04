278682c0bb4f5a64173cb688997d4f9be847c4f2
shadowgamersamyt-gif <shadowgamersamyt@gmail.com>
2025-10-04 11:56:13 -0400

    new

--- bot.py
+++ bot.py
@@ -8,6 +8,22 @@ from psycopg2.extras import RealDictCursor
 from datetime import datetime, timedelta
 import json
 import asyncio
+import os
+from flask import Flask
+from threading import Thread
+
+# Flask keep-alive server
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive!"
+
+def run():
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+# Start Flask server in background thread
+Thread(target=run).start()
 
 intents = discord.Intents.default()
 intents.members = True
